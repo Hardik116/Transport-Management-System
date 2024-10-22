68aa00d2e64ca71426fec3b0f2c7513117329ff7
Hardik116 <96564448+Hardik116@users.noreply.github.com>
2024-10-22 18:22:41 +0530

    final

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -106,7 +106,7 @@ const Navbar = () => {
           </Link>
         )}
         {
-          user?.email === "abc@gmail.cpm" && 
+          user?.email === "admin@gmail.com" && 
           <button
           onClick={handleAdmin}
           className="bg-white mx-3 text-black px-4 py-2 rounded-md hover:bg-gray-200 transition duration-300"
